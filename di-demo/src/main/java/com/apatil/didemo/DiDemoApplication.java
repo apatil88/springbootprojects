ff8342b94c6f0572e4d4b2a81cee58e5822710ec
apatil88 <amrut.patil88@hotmail.com>
2018-08-22 07:06:30 -0400

    Property DI, Setter DI, Constructor DI.

---
+++ di-demo/src/main/java/com/apatil/didemo/DiDemoApplication.java
@@ -0,0 +1,27 @@
+package com.apatil.didemo;
+
+import com.apatil.didemo.controllers.ConstructorInjectedController;
+import com.apatil.didemo.controllers.MyController;
+import com.apatil.didemo.controllers.PropertyInjectedController;
+import com.apatil.didemo.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		myController.sayHello();
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+	}
+}
